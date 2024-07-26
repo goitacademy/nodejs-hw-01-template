dd149be9ead7cc21830ecdbdd52ecb56c7e47d33
Alexander Repeta <alexander.repeta@gmail.com>
2024-07-26 21:12:36 +0300

    Update createFakeContact.js

--- src/utils/createFakeContact.js
+++ src/utils/createFakeContact.js
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker";
 
 export const createFakeContact = () => ({
-  id: faker.datatype.uuid(),
+  id: faker.string.uuid(),
   name: faker.person.fullName(),
   phone: faker.phone.number(),
   email: faker.internet.email(),
