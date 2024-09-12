67978c71f89e67307e5e78088343cde4c4d0bdeb
NataD81 <dvoryanchuk81@gmail.com>
2024-09-12 21:56:08 +0300

    getAllContacts.js

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,10 +1,19 @@
-
-
-
 export const getAllContacts = async () => {
-    return ["Joann Kuphal","Ramiro Dickens"];
+    return [
+        { id: "86cf69d4-8e8c-490f-bd0c-163ff0278a12",
+    name: "Nick Kassulke",
+    phone: "(247) 612-2678 x1129",
+    email: "Devon31@yahoo.com",
+    job: "Dynamic Marketing Director"
+    },
+    {
+        id: "9e0fe2ce-68e9-4867-99d3-4480898c0980",
+        name: "Ernestine Bashirian",
+        phone: "(573) 969-6762 x9082",
+        email: "Earnestine12@yahoo.com",
+        job: "Regional Program Designer"
+      },
+    ];
 };
-
-
 
 console.log(await getAllContacts());
