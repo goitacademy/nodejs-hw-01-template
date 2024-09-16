4d1c683d7ee45692928ffc7579b9c9aec77655fb
NataD81 <dvoryanchuk81@gmail.com>
2024-09-16 19:49:48 +0300

    update getAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,19 +1,13 @@
+import { readContacts } from "../utils/readContacts.js";
 export const getAllContacts = async () => {
-    return [
-        { id: "86cf69d4-8e8c-490f-bd0c-163ff0278a12",
-    name: "Nick Kassulke",
-    phone: "(247) 612-2678 x1129",
-    email: "Devon31@yahoo.com",
-    job: "Dynamic Marketing Director"
-    },
-    {
-        id: "9e0fe2ce-68e9-4867-99d3-4480898c0980",
-        name: "Ernestine Bashirian",
-        phone: "(573) 969-6762 x9082",
-        email: "Earnestine12@yahoo.com",
-        job: "Regional Program Designer"
-      },
-    ];
+    try {
+        const contacts =await readContacts();
+        return contacts;
+    } catch (error) {
+        console.error('Помилка:', error);
+        return [];
+    }
+
 };
 
 console.log(await getAllContacts());
