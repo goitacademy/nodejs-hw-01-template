f9618a1b617b16136eb47f492f071fcd8c4a7217
Nataliia Babina <157317342+Nataliia-Babina@users.noreply.github.com>
2024-10-08 21:01:49 +0200

    nodejs-hw

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,14 @@
-export const getAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await getAllContacts());
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const contacts = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+  return contacts;
+};
